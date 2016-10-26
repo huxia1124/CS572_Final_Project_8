839e85221225adf816966b824f6270851c6b475d
sugir05 <sugirthan05@gmail.com>
2016-10-25 20:02:03 -0500

    add course view file

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var router = express.Router();
+require('../mongoconnection');
+var Course = require('../model/courseSchema');
 
 //Api implementations
 router.get('/', function(req, res, next) {
-    res.send('course de');
+    Course.find(function (err, courses) {
+        if(err){
+            res.send(err);
+        }
+        res.json(courses);
+    })
 });
 
+router.get('/allcourses', function(req, res, next) {
+    //checkConnection();
+    Course.find({}, function(err, courses) {
+        if(err) throw err;
+        res.render('allcourse.jade', {courses: courses});
+    })
+});
 
+router.get('/:id', function(req, res, next) {
+    Course.findOne(function (err, courses) {
+        if(err){
+            res.send(err);
+        }
+        res.json(courses);
+    })
+});
 
 module.exports = router;
